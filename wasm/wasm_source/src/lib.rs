03129528d70c8c9fb7b50fd55b232aa0f0f7bbaf
Tomáš Zemanovič <tzemanovic@gmail.com>
2021-08-31 13:28:59 +0200

    [wasm] add txs for PoS bond, unbond & withdraw

--- wasm/wasm_source/src/lib.rs
+++ wasm/wasm_source/src/lib.rs
@@ -4,6 +4,181 @@ pub mod mm_token_exch;
 #[cfg(feature = "vp_user")]
 pub mod vp_user;
 
+/// A tx for a PoS bond that stakes tokens via a self-bond or delegation.
+#[cfg(feature = "tx_bond")]
+pub mod tx_bond {
+    use anoma_vm_env::tx_prelude::proof_of_stake::BondId;
+    use anoma_vm_env::tx_prelude::*;
+
+    #[transaction]
+    fn apply_tx(tx_data: Vec<u8>) {
+        let signed =
+            key::ed25519::SignedTxData::try_from_slice(&tx_data[..]).unwrap();
+        let bond =
+            pos::Bond::try_from_slice(&signed.data.unwrap()[..]).unwrap();
+
+        // TODO temporary for logging:
+        let bond_id = BondId {
+            source: bond.source.as_ref().unwrap_or(&bond.validator).clone(),
+            validator: bond.validator.clone(),
+        };
+        let bond_pre = PoS.read_bond(&bond_id);
+        let validator_set_pre = PoS.read_validator_set();
+        let total_deltas_pre = PoS.read_validator_total_deltas(&bond.validator);
+        let vp_pre = PoS.read_validator_voting_power(&bond.validator);
+
+        let epoch = get_block_epoch();
+        if let Err(err) = PoS.bond_tokens(
+            bond.source.as_ref(),
+            &bond.validator,
+            bond.amount,
+            epoch,
+        ) {
+            log_string(format!("Bond failed with: {}", err));
+            panic!()
+        }
+
+        // TODO temporary for logging:
+        let bond_post = PoS.read_bond(&bond_id);
+        let validator_set_post = PoS.read_validator_set();
+        let total_deltas_post =
+            PoS.read_validator_total_deltas(&bond.validator);
+        let vp_post = PoS.read_validator_voting_power(&bond.validator);
+        log_string(format!("bond pre {:#?}, post {:#?}", bond_pre, bond_post));
+        log_string(format!(
+            "validator set pre {:#?}, post {:#?}",
+            validator_set_pre, validator_set_post
+        ));
+        log_string(format!(
+            "validator total deltas pre {:#?}, post {:#?}",
+            total_deltas_pre, total_deltas_post
+        ));
+        log_string(format!(
+            "validator voting power pre {:#?}, post {:#?}",
+            vp_pre, vp_post
+        ));
+    }
+}
+
+/// A tx for a PoS unbond that removes staked tokens from a self-bond or a
+/// delegation to be withdrawn in or after unbonding epoch.
+#[cfg(feature = "tx_unbond")]
+pub mod tx_unbond {
+    use anoma_vm_env::tx_prelude::proof_of_stake::BondId;
+    use anoma_vm_env::tx_prelude::*;
+
+    #[transaction]
+    fn apply_tx(tx_data: Vec<u8>) {
+        let signed =
+            key::ed25519::SignedTxData::try_from_slice(&tx_data[..]).unwrap();
+        let unbond =
+            pos::Unbond::try_from_slice(&signed.data.unwrap()[..]).unwrap();
+
+        // TODO temporary for logging:
+        let bond_id = BondId {
+            source: unbond.source.as_ref().unwrap_or(&unbond.validator).clone(),
+            validator: unbond.validator.clone(),
+        };
+        let bond_pre = PoS.read_bond(&bond_id);
+        let unbond_pre = PoS.read_unbond(&bond_id);
+        let validator_set_pre = PoS.read_validator_set();
+        let total_deltas_pre =
+            PoS.read_validator_total_deltas(&unbond.validator);
+        let vp_pre = PoS.read_validator_voting_power(&unbond.validator);
+        let total_vp_pre = PoS.read_total_voting_power();
+
+        let epoch = get_block_epoch();
+        if let Err(err) = PoS.unbond_tokens(
+            unbond.source.as_ref(),
+            &unbond.validator,
+            unbond.amount,
+            epoch,
+        ) {
+            log_string(format!("Unbonding failed with: {}", err));
+            panic!()
+        }
+
+        // TODO temporary for logging:
+        let bond_post = PoS.read_bond(&bond_id);
+        let unbond_post = PoS.read_unbond(&bond_id);
+        let validator_set_post = PoS.read_validator_set();
+        let total_deltas_post =
+            PoS.read_validator_total_deltas(&unbond.validator);
+        let vp_post = PoS.read_validator_voting_power(&unbond.validator);
+        let total_vp_post = PoS.read_total_voting_power();
+        log_string(format!("bond pre {:#?}, post {:#?}", bond_pre, bond_post));
+        log_string(format!(
+            "unbond pre {:#?}, post {:#?}",
+            unbond_pre, unbond_post
+        ));
+        log_string(format!(
+            "validator set pre {:#?}, post {:#?}",
+            validator_set_pre, validator_set_post
+        ));
+        log_string(format!(
+            "validator total deltas pre {:#?}, post {:#?}",
+            total_deltas_pre, total_deltas_post
+        ));
+        log_string(format!(
+            "validator voting power pre {:#?}, post {:#?}",
+            vp_pre, vp_post
+        ));
+        log_string(format!(
+            "total voting power pre {:#?}, post {:#?}",
+            total_vp_pre, total_vp_post
+        ));
+    }
+}
+
+/// A tx for a PoS unbond that removes staked tokens from a self-bond or a
+/// delegation to be withdrawn in or after unbonding epoch.
+#[cfg(feature = "tx_withdraw")]
+pub mod tx_withdraw {
+    use anoma_vm_env::tx_prelude::proof_of_stake::BondId;
+    use anoma_vm_env::tx_prelude::*;
+
+    #[transaction]
+    fn apply_tx(tx_data: Vec<u8>) {
+        let signed =
+            key::ed25519::SignedTxData::try_from_slice(&tx_data[..]).unwrap();
+        let withdraw =
+            pos::Withdraw::try_from_slice(&signed.data.unwrap()[..]).unwrap();
+
+        // TODO temporary for logging:
+        let bond_id = BondId {
+            source: withdraw
+                .source
+                .as_ref()
+                .unwrap_or(&withdraw.validator)
+                .clone(),
+            validator: withdraw.validator.clone(),
+        };
+        let unbond_pre = PoS.read_unbond(&bond_id);
+
+        let epoch = get_block_epoch();
+        match PoS.withdraw_tokens(
+            withdraw.source.as_ref(),
+            &withdraw.validator,
+            epoch,
+        ) {
+            Ok(slashed) => {
+                log_string(format!("Withdrawal slashed for {}", slashed));
+            }
+            Err(err) => {
+                log_string(format!("Withdrawal failed with: {}", err));
+                panic!()
+            }
+        }
+
+        // TODO temporary for logging:
+        let unbond_post = PoS.read_unbond(&bond_id);
+        log_string(format!(
+            "unbond pre {:#?}, post {:#?}",
+            unbond_pre, unbond_post
+        ));
+    }
+}
+
 /// A tx for a token transfer crafted by matchmaker from intents.
 /// This tx uses `intent::IntentTransfers` wrapped inside
 /// `key::ed25519::SignedTxData` as its input as declared in `shared` crate.
@@ -94,15 +269,15 @@ pub mod vp_token {
 
     #[validity_predicate]
     fn validate_tx(
-        tx_data: Vec<u8>,
+        _tx_data: Vec<u8>,
         addr: Address,
         keys_changed: HashSet<storage::Key>,
         verifiers: HashSet<Address>,
     ) -> bool {
         log_string(format!(
-            "validate_tx called with token addr: {}, key_changed: {:#?}, \
-             tx_data: {:#?}, verifiers: {:?}",
-            addr, keys_changed, tx_data, verifiers
+            "validate_tx called with token addr: {}, key_changed: {:?}, \
+             verifiers: {:?}",
+            addr, keys_changed, verifiers
         ));
 
         token::vp(&addr, &keys_changed, &verifiers)
